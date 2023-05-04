0819a2b1c0baff06c283c18fc1af419b8de7dcd8
psoper1 <107810987+psoper1@users.noreply.github.com>
2023-05-04 00:30:22 +0000

    blank

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,13 +1,20 @@
 import './App.css';
 
 function App() {
+
+  const handleClick = () => {
+    alert("You pressed the button!")
+  }
+
   return (
     <>
     <div>
       Test
     </div>
     <div>Test 2</div>
-    <button className="btn btn-primary">Bootstrap Button</button>
+    <div className='text-center'>
+    <button onClick={handleClick} className="btn btn-primary">Bootstrap Button</button>
+    </div>
     </>
   );
 }
